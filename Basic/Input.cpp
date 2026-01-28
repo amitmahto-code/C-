496f14a656a7944139dd3fa41d8acd9040d66923
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-28 23:45:50 +0530

    how to get input from user

---
+++ Basic/Input.cpp
@@ -0,0 +1,10 @@
+// geting input from the user
+
+#include<iostream>
+using namespace std ;
+int main(){
+    int x;
+    cout<<"Enter number :- ";
+    cin>>x;
+    cout<<"Square of number is:- "<<x*x;
+}
