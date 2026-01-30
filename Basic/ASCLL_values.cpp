2ba3e1ebdf9c5fc3625797ffef17bed9e22dc0ad
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-30 22:41:03 +0530

    ASCLL values

---
+++ Basic/ASCLL_values.cpp
@@ -0,0 +1,8 @@
+// using typecating we know the ascll values
+#include<iostream>
+using namespace std;
+int main(){
+    char ch;
+    cout<<"enter charcter ";cin>>ch;
+    cout<<"ASCLL value "<<(int)ch;
+}
