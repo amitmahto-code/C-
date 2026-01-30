bca42c5ec40ead32c6977549ea2802732f1eead8
amitmahto-code <mahtoamit6678@gmail.com>
2026-01-30 22:55:31 +0530

    sum of number

---
+++ Basic/sum_of_number.cpp
@@ -0,0 +1,12 @@
+// sum of two number using user input 
+#include<iostream>
+using namespace std;
+int main(){
+    int x,y;
+    cout<<"Enter first number:- ";
+    cin>>x;
+    cout<<"Enter second number:- ";
+    cin>>y;
+    cout<<"Sum of two number:- "<<x+y;
+    
+}
