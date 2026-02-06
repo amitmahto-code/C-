88e6cde5fcbb8d12a09bd2ceecfe09e88cffd99a
amitmahto-code <mahtoamit6678@gmail.com>
2026-02-07 00:47:42 +0530

    calculate simple interest

---
+++ Basic/Calculation_simple_interest.cpp
@@ -0,0 +1,12 @@
+// Calculation simpe intrest
+#include<iostream>
+using namespace std ;
+int main(){
+    int Principal=100;
+    float interest =11.98;
+    float time = 2;
+    float SI = (Principal*interest*time)/100;
+    cout<<"Simple interest :- "<<SI ;
+
+
+}
