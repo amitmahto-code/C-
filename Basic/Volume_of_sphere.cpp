86db4af5bc26ed45363f0f5fdf87641d4cc65e58
Mahto Amit <mahtoamit6678@gmail.com>
2026-06-01 16:32:30 +0530

    Calculation volume of sphere

---
+++ Basic/Volume_of_sphere.cpp
@@ -0,0 +1,10 @@
+// Calculation volume of sphere
+#include<iostream>
+using namespace std ;
+int main(){
+    float r=10;
+    float pi=3.14;
+    int v= (4.0/3)*pi*r*r*r;
+    cout<<"volume of sphere:- "<<v;
+
+}
