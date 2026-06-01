ff9d51b26b4b02fc2b547780c3bbce4a411b9053
Mahto Amit <mahtoamit6678@gmail.com>
2026-06-01 16:35:16 +0530

    integer number and outpu that number even or odd

---
+++ Chapter 2/if_condition.cpp
@@ -0,0 +1,16 @@
+// write a program that input integer number and outpu that number even or odd;
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter number: ";
+    cin>>n;
+    if (n%2==0){
+        
+        cout<<"Even Number ";
+    }
+    else{
+        cout<<"Odd Number";
+    }
+    return 0;
+} 
