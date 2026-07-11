95738a79cbeb526d3d131407a43ccd1def3cd9a2
Mahto Amit <mahtoamit6678@gmail.com>
2026-07-11 16:32:32 +0530

    default constructor

---
+++ clg/p3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+class student{
+        int roll;
+    public:
+        student(){
+            roll=10;
+            cout<<roll<<endl;
+        }
+};
+
+int main(){
+    student s1,s2;
+}
