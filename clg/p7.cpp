2e0327a102e2cc10bbafbce8fa2d30ed79bd5ab7
Mahto Amit <mahtoamit6678@gmail.com>
2026-07-15 20:19:22 +0530

    friends function program

---
+++ clg/p7.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class x{
+    private:
+        int a = 5;
+        friend class y;
+};
+
+class y {
+    public:
+        void show(x obj){
+            cout<<obj.a<<endl;
+        }
+};
+
+int main(){
+    x X;
+    y Y ;
+    Y.show(X);
+    return 0;
+
+}
