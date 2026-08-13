fc9892f030afb8447b920c8ee47c6e419ac20ea4
Mahto Amit <mahtoamit6678@gmail.com>
2026-08-13 18:02:58 +0530

    Character array Problem

---
+++ Problems/p3.cpp
@@ -0,0 +1,12 @@
+// Write a program for character array. 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    char name[20];
+    cout<<"Enter array: ";
+    cin>>name;
+    cout<<name;
+    return 0;
+}
